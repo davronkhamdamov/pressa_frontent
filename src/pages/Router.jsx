df99ebbf52d9a8b507171d9034b8a25837f10cb0
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-18 05:42:43 +0500

    login complated

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 const Home = React.lazy(() => import("./Home/Home"));
 const Admin = React.lazy(() => import("./Admin/Admin"));
@@ -7,11 +7,14 @@ const Annaunced = React.lazy(() => import("./annaunced/Annaounced"));
 const PageNotFound = React.lazy(() => import("./PageNotFound/PageNotFound"));
 
 const Router = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/login" element={<Login />} />
+      <Route
+        path="/admin"
+        element={isAdmin ? <Admin /> : <Login setIsAdmin={setIsAdmin} />}
+      />
       <Route path="/annaunced" element={<Annaunced />} />
       <Route path="/*" element={<PageNotFound />} />
     </Routes>
