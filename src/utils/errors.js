ea8fb0f34a775a1888ac702bd5acfa2b0f0e5be8
davronkhamdamov <xamdamovdavron6@gmail.com>
2023-05-22 10:37:59 +0500

    errors list added

---
+++ src/utils/errors.js
@@ -0,0 +1,14 @@
+const errors = [
+    'announcement.description cannot be null',
+    'announcement.link cannot be null',
+    'Validation is on phone_number failed',
+    'announcement.fullname cannot be null',
+    'announcement.subject_text cannot be null',
+    'announcement.time cannot be null',
+    'announcement.yonalish cannot be null',
+    'announcement.ichki_yonalish cannot be null',
+    'Validation is on title failed',
+    'Validation isUrl on img_url failed',
+    'Validation is on description failed'
+]
+export default errors
